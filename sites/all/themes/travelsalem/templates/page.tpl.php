da625be1c00853bf0e00526633db1fde1c33a424
Daniel Deitschel <daniel@gocosmonaut.com>
2016-01-03 22:36:31 -0800

    Footer added
    526

--- sites/all/themes/travelsalem/templates/page.tpl.php
+++ sites/all/themes/travelsalem/templates/page.tpl.php
@@ -126,7 +126,7 @@
   <div class="row">
 
     <?php if (!empty($page['sidebar_first'])): ?>
-      <aside class="col-sm-3" role="complementary">
+      <aside class="col-sm-3 left" role="complementary">
         <?php print render($page['sidebar_first']); ?>
       </aside>  <!-- /#sidebar-first -->
     <?php endif; ?>
@@ -156,7 +156,7 @@
     </section>
 
     <?php if (!empty($page['sidebar_second'])): ?>
-      <aside class="col-sm-3" role="complementary">
+      <aside class="col-sm-3 right" role="complementary">
         <?php print render($page['sidebar_second']); ?>
       </aside>  <!-- /#sidebar-second -->
     <?php endif; ?>
